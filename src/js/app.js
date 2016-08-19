eb9973ec294e57707181d86e373ca92876750963
bmcminn <labs@gbox.name>
2016-08-19 03:05:47 -0500

    structure App instances with Event bus system for cross modules comm; intial working version of model querying OMDB API; rendering results to view

--- src/js/app.js
+++ src/js/app.js
@@ -1,82 +1,185 @@
-var Person   = {}
-,   Dale     = {}
-,   Bob      = {}
-;
 
-(function() {
-
-    Person = Backbone.Model.extend({
-
-        defaults: {
-            name:       'John Doe'
-        ,   age:        30
-        ,   occupation: 'Mouth Feeler'
-        }
-
-    ,   events: {
-            // 'change': 'onChange'
-            'error':    'onError'
-        ,   'invalid':  'onInvalid'
-        }
+// Update our Underscore instance to use Mustache style tag delimiters
+_.templateSettings.escape       = /\{\{-(.*?)\}\}/g;
+_.templateSettings.interpolate  = /\{\{(.+?)\}\}/g;
 
 
-    ,   work: function() {
-            return this.get('name') + ' is ' + this.get('occupation') + 'ing!';
-        }
+// Initialize our application instance object
+var App = {};
 
-    ,   validate: function(attrs, options) {
-            console.log(attrs);
-            if (attrs.age <= 0) {
-                return 'Age must be positive!!';
+
+// Initialize our Application's event bus
+App.vent = _.extend({}, Backbone.Events);
+
+
+App.Collections = {};
+App.Models      = {};
+App.Views       = {};
+
+
+App.Collections.SearchableCollection = Backbone.Collection.extend({}, {
+    search: function(query, options) {
+        var search = $.Deferred();
+
+        options = options || {};
+
+        var collection = new this([], options);
+
+        collection.url = _.result(collection, 'url');
+
+        var fetch = collection.fetch({
+            data: {
+                s: query
             }
+        });
 
-            if (!attrs.name) {
-                return 'Name must be filled in!!';
-            }
-        }
+        fetch.done(_.bind(function() {
+            App.vent.trigger('search:done');
+            search.resolveWith(this.Search, [collection]);
+        }, this));
 
-    // ,   onChange: function(e) {
-    //         console.log(e);
-    //         if (e.attributes.age)
-    //     }
-
-    ,   onError: function(model, err) {
-            console.log('onError()');
-            console.log(err);
-            return this.validationError;
+        fetch.fail(function() {
+            App.vent.trigger('search:fail');
+            search.reject();
         })
 
-    ,   onInvalid: function(model, err) {
-            console.log('onInvalid()');
-            console.log(err);
-            return this.validationError;
-        })
+        return search.promise();
+    }
+});
 
+
+var MovieResults = App.Collections.SearchableCollection.extend({
+    url: 'http://omdbapi.com/'
+}) ;
+
+
+
+
+
+
+
+// Individual movie model
+var Movie = Backbone.Model.extend();
+
+
+
+// Resulting Movies collection
+var Movies = Backbone.Collection.extend({
+    model: Movie
+});
+
+
+
+// Individual movie listing view
+var MovieView = Backbone.View.extend({
+    tagName: 'li'
+
+
+,   events: {
+        click: 'onClick'
+    }
+
+
+,   template: _.template($('#movie').html())
+
+
+,   initialize: function(options) {
+        this.bus = options.bus;
+    }
+
+
+,   onClick: function() {
+        this.bus.trigger('movie:selected', this.model);
+    }
+
+
+,   render: function() {
+
+        console.log(this.model);
+
+        this.$el.html(this.template(this.model));
+
+        return this;
+    }
+});
+
+
+
+// Movie listings view
+var MoviesView = Backbone.View.extend({
+    tagName:    'ul'
+
+,   el:         '#movies'
+
+,   initialize: function(options) {
+        this.bus = options.bus;
+
+    }
+
+,   render: function() {
+
+        // we alias 'this' to 'self' so we have a reference to 'this' if we change lexical scope
+        var self = this;
+
+        this.collection.each(function(model) {
+
+            var movies = model.toJSON().Search;
+
+            movies.forEach(function(movie) {
+                var view = new MovieView({
+                    model: movie
+                ,   bus: self.bus
+                });
+
+                self.$el.append(view.render().$el);
+            });
+
+        });
+
+        return this;
+    }
+});
+
+
+
+
+// var MovieDetailsView = Backbone.View.extend({
+//     el: '#movie-details'
+
+// ,   initialize: function(options) {
+//         this.bus = options.bus;
+//         this.bus.on('movie:selected', this.onMovieSelected, this);
+//     }
+
+// ,   render: function() {
+//         if (this.model) {
+//             this.$('#movie-name').html(this.model.get('name'));
+//         }
+
+//         return this;
+//     }
+
+
+// ,   onMovieSelected: function(movie) {
+//         this.model = movie;
+//         this.render();
+//         console.log('movie selected', data);
+//     }
+// }) ;
+
+
+
+
+
+
+var findMovies = MovieResults.search('robin hood');
+
+findMovies.done(function(movies) {
+
+    // console.log(movies);
+    var moviesView = new MoviesView({
+        collection: movies
     });
 
-
-
-
-
-    Bob = new Person({
-        name:       'Bob Barker'
-    ,   age:        73
-    ,   occupation: 'Game show Announcer'
-    });
-
-
-    Dale = new Person({
-        name: 'Dale Gribble'
-    ,   age: 43
-    ,   occupation: 'Soldier of Fortune'
-    });
-
-
-    // Bob.on('invalid', function(model, err) {
-    //     console.log(err);
-    //     console.log('BLAH, THERE WAS A PROBLEM!!!');
-    // });
-
-
-
-})();
+    moviesView.render();
+});
